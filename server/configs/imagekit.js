2d678562fb761e6deb3e6c8d3ca17001bc2c9732
Subharthy Kuiry <144170747+S8kuiry@users.noreply.github.com>
2025-10-29 11:55:52 +0530

    Update imagekit.js

--- server/configs/imagekit.js
+++ server/configs/imagekit.js
@@ -1,16 +1,11 @@
-
-
-
-
-import ImageKit from '@imagekit/nodejs';
-import dotenv from 'dotenv'
-dotenv.config()
-
+import { ImageKit } from "@imagekit/nodejs"; // <-- FIXED import
+import dotenv from "dotenv";
+dotenv.config();
 
 const imagekit = new ImageKit({
-   publicKey : process.env.IMAGEKIT_PUBLIC_KEY,
-    privateKey :process.env.IMAGEKIT_PRIVATE_KEY,
-    urlEndpoint : process.env.IMAGEKIT_URL_ENDPOINT// This is the default and can be omitted
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 });
 
-export default imagekit
+export default imagekit;
